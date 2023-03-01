0a62bdf1e8448fc0c74198858762b122931316e9
Ubuntu <wilcamsdington>
2023-03-01 23:03:20 +0000

    test_V1

--- Pre_processing.py
+++ Pre_processing.py
@@ -9,10 +9,7 @@ import pandas as pd
 with open('data/RC_2006-01') as f:
     data = pd.read_json(f, lines=True)
 
-
 # general pre processing
-data = data[data['body'].str.len() >= 10] # drop len(body) less than 10 
-data = data.drop(data[data['body'] == '[deleted]'].index)  # drop body that deleted
 
 
 # In[2]:
